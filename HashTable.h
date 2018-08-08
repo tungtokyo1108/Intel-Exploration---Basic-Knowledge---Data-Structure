397287d00c4cf0605768cb048052aba80c59c87e
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-08-09 00:23:28 +0900

    Update HashTable.h

--- HashTable.h
+++ HashTable.h
@@ -404,56 +404,396 @@ void CHashTableType::DeleteHashNode(CHashNode* pHashNode) {
 	}
 }
 
+template<HashTableTemplateList>
+DWORD CHashTableType::GetHashSize( const DWORD size ) const
+{
+    // the size of the hash table array is a large prime number near but
+    // smaller than the maximum size of the cache but not near a power of two
+    if( size >= 6151 )
+    {
+        return 6151;
+    }
+    else if( size >= 3079 )
+    {
+        return 3079;
+    }
+    else if( size >= 1543 )
+    {
+        return 1543;
+    }
+    else if( size >= 769 )
+    {
+        return 769;
+    }
+    else if( size >= 389 )
+    {
+        return 389;
+    }
+    else if( size >= 193 )
+    {
+        return 193;
+    }
+    else if( size >= 97 )
+    {
+        return 97;
+    }
+    else
+    {
+        return 53;
+    }
+}
+
+template<HashTableTemplateList>
+DWORD CHashTableType::GetHashIndex(const CHashKey &hasKey) const {
+	return (hasKey.GetHashCode() % m_HashArray.GetSize());
+}
+
+template<HashTableTemplateList>
+CHashTableType::CHashKey::CHashKey(void) : CObject<CAllocatorType>() {
+	SafeMemSet(&m_KeyValue,0,sizeof(KeyType));
+	m_HashCode = 0;
+}
+
+template<HashTableTemplateList>
+CHashTableType::CHashKey::CHashKey(const KeyType &keyValue) : CObject<CAllocatorType>() {
+#ifndef _WIN32 && _MSC_VER
+	ASSERT(IsAligned(&m_KeyValue,sizeof(DQWORD)));
+	ASSERT(IsAligned(&m_HashCode,sizeof(DQWORD)));
+
+	m_KeyValue = keyValue;
+	ASSERT(m_KeyValue == keyValue);
+
+	if (sizeof(KeyValue) < sizeof(DQWORD))
+	{
+		const DWORD* pKeyValue = (const DWORD*)&m_KeyValue;
+		m_HashCode = *pKeyValue++;
+
+		const DWORD* count = (DWORD)(sizeof(KeyType)/sizeof(DWORD));
+		for (DWORD i=1; i < count; ++i)
+		{
+			const DWORD data = *pKeyValue++;
+			m_HashCode = (m_HashCode << 1) ^ data;
+		}
+	}
+	else
+	{
+		const DWORD szAlignedKeyValue = (DWORD)((((sizeof(KeyType) - 1) / sizeof(DQWORD)) + 1) * sizeof(DQWORD));
+		const DWORD szPad = szAlignedKeyValue - (DWORD)sizeof(KeyType);
+
+		if(szPad)
+		{
+			SafeMemSet((BYTE*)&m_KeyValue + sizeof(KeyValue), 0, szPad);
+		}
+
+		const __m128i* pKeyValue = (const __m128i*)&m_KeyValue;
+		__m128i* hash = _mm_load_si128(pKeyValue++);
+
+		const DWORD count = szAlignedKeyValue / sizeof(DQWORD);
+		for (DWORD i=1; i<count; i++)
+		{
+			const __m128i data = _mm_load_si128(pKeyValue++);
+			hash = _mm_xor_si128(_mm_slli_si128(hash,1),data);
+		}
+
+		hash = _mm_xor_si128(_mm_unpackhi_epi32(hash,hash), _mm_unpacklo_epi32(hash,hash));
+		hash = _mm_xor_si128(_mm_shuffle_epi32(hash,0x2), hash);
+		m_HashCode = _mm_cvtsi128_si32(hash);
+	}
+#endif
+
+	ASSERT(IsAligned(&m_KeyValue, sizeof(DWORD)));
+	ASSERT(IsAligned(&m_HashCode, sizeof(DWORD)));
+
+	m_KeyValue = keyValue;
+	ASSERT(m_KeyValue == keyValue);
+
+	const DWORD* pKeyValue = (const DWORD*)&m_KeyValue;
+	m_HashCode = *pKeyValue++;
+
+	const DWORD count = (DWORD)(sizeof(KeyValue)/sizeof(DWORD));
+	for (DWORD i=1; i<count; i++)
+	{
+		const DWORD data = *pKeyValue++;
+		m_HashCode = (m_HashCode << 1) ^ data;
+	}
+}
+
+template<HashTableTemplateList>
+CHashTableType::CHashKey::CHashKey(const KeyType &keyValue, const DWORD hashCode) : CObject<CAllocatorType>() {
+	m_KeyValue = keyValue;
+	ASSERT(m_KeyValue == keyValue);
+	m_HashCode = hashCode;
+}
+
+template<HashTableTemplateList>
+CHashTableType::CHashKey::CHashKey(const CHashKey &hashKey) {
+	m_keyValue = hashKey.m_KeyValue;
+	m_HashCode = hashKey.m_HashCode;
+}
+
+template<HashTableTemplateList>
+CHashTableType::CHashKey::~CHashKey(void) {}
+
+template<HashTableTemplateList>
+DWORD CHashTableType::CHashKey::GetHashCode(void) const {
+	return m_HashCode;
+}
+
+template<HashTableTemplateList>
+const KeyType& CHashTableType::CHashKey::GetKeyValue(void) const {
+	return m_KeyValue;
+}
+
+template<HashTableTemplateList>
+inline bool CHashTableType::CHashKey::operator ==(const CHashKey &hashKey) const {
+	if (m_HashCode == hashKey.m_HashCode)
+	{
+		return (m_KeyValue == hashKey.m_KeyValue);
+	}
+	else
+	{
+		return false;
+	}
+}
+
+template<HashTableTemplateList>
+CHashTableType::CHashNode::CHashNode (const CHashKey &hashKey, const ElementType &element) : CObject<CAllocatorType>(), m_HashKey(hashKey) {
+	m_Element = element;
+}
+
+template<HashTableTemplateList>
+CHashTableType::CHashNode::~CHashNode(void) {}
+
+template<HashTableTemplateList>
+const typename CHashTableType::CHashKey& CHashTableType::CHashNode::GetHashKey(void) const {
+	return m_HashKey;
+}
+
+template<HashTableTemplateList>
+ElementType CHashTableType::CHashNode::GetElement(void) const {
+	return m_Element;
+}
+
+template<HashTableTemplateList>
+CHashTableType::CIterator::CIterator(void) : m_Iterator() {
+	m_Array = NULL;
+	m_ArrayIndex = 0;
+}
+
+template<HashTableTemplateList>
+CHashTableType::CIterator::CIterator(const CIterator &iterator) : m_Iterator(iterator.m_Iterator) {
+	m_Array = iterator.m_Array;
+	m_ArrayIndex = iterator.m_ArrayIndex;
+}
+
+template<HashTableTemplateList>
+typename CHashTableType::CIterator& CHashTableType::CIterator::operator --(void) {
+	--m_Iterator;
+	CHashTableKeyList* pHashKeyList = m_Array->GetElement(m_ArrayIndex);
+	ASSERT(pHashKeyList);
+
+	while((m_Iterator == pHashKeyList->End()) && (m_ArrayIndex != 0))
+	{
+		CHashTableKeyList* pPrevHashKeyList = m_Array->GetElement(--m_ArrayIndex);
+		if (pPrevHashKeyList)
+		{
+			pHashKeyList = pPrevHashKeyList;
+			m_Iterator = pHashKeyList->End();
+			--m_Iterator;
+		}
+	}
+	return *this;
+}
+
+template<HashTableTemplateList>
+typename CHashTableType::CIterator& CHashTableType::CIterator::operator ++(void) {
+	++m_Iterator;
+	CHashTableKeyList* pHashKeyList = m_Array->GetElement(m_ArrayIndex);
+	ASSERT(pHashKeyList);
+
+	const DWORD maxIndex = m_Array->GetSize() - 1;
+	while ((m_Iterator == pHashKeyList->End()) && (m_ArrayIndex != maxIndex))
+	{
+		CHashTableKeyList* pNextHashKeyList = m_Array->GetElement(++m_ArrayIndex);
+		if (pNextHashKeyList)
+		{
+
+		}
+	}
+}
+
+template<HashTableTemplateList>
+bool CHashTableType::CIterator::operator ==(const CIterator& iterator) const {
+	return m_Iterator == iterator.m_Iterator;
+}
+
+template<HashTableTemplateList>
+bool CHashTableType::CIterator::operator !=(const CIterator& iterator) const {
+	return m_Iterator != iterator.m_Iterator;
+}
+
+template<HashTableTemplateList>
+typename CHashTableType::CIterator& CHashTableType::CIterator::operator =(const CIterator &iterator) {
+	m_Array = iterator.m_Array;
+	m_ArrayIndex = iterator.m_ArrayIndex;
+	m_Iterator = iterator.m_Iterator;
+	return *this;
+}
+
+template<HashTableTemplateList>
+ElementType CHashTableType::CIterator::operator *(void) {
+	return (*m_Iterator)->GetElement();
+}
+
+template<HashTableTemplateList>
+const KeyType& CHashTableType::CIterator::GetKeyValue(void) {
+	return (*m_Iterator)->GetHasKey().GetKeyValue();
+}
+
+template<HashTableTemplateList>
+typename CHashTableType::CIterator CHashTableType::Begin(void) const {
+	CIterator iterator;
+
+	const DWORD size = m_HashArray.GetSize();
+	for (DWORD i=0; i<size; ++i)
+	{
+		CHashTableKeyList* pHashKeyList = m_HashArray.GetElement(i);
+		if (pHashKeyList)
+		{
+			iterator.m_Iterator = pHashKeyList->Begin();
+			iterator.m_Array = const_cast<CHashTableArray*>(&m_HashArray);
+			iterator.m_ArrayIndex = i;
+			return iterator;
+		}
+	}
+	return iterator;
+}
+
+template<HashTableTemplateList>
+typename CHashTableType::CIterator CHashTableType::End(void) const {
+	CIterator iterator;
+	const DWORD size = m_HashArray.GetSize();
+	for (int i=(int)size - 1; i >= 0; --i)
+	{
+		CHashTableKeyList* pHashKeyList = m_HashArray.GetElement((DWORD)i);
+		if (pHashKeyList)
+		{
+			iterator.m_Iterator = pHashKeyList->End();
+			iterator.m_Array = const_cast<CHashTableArray*>(&m_HashArray);
+			iterator.m_ArrayIndex = (DWORD)i;
+			return iterator;
+		}
+	}
+	return iterator;
+}
+
+template<HashTableTemplateList>
+bool CHashTableType::Add(const CHashKey &hashKey, const ElementType &element, CIterator &iterator) {
+	if (m_HashArray.GetSize() == 0)
+	{
+		return false;
+	}
+	const DWORD index = GetHashIndex(hashKey);
+	ASSERT(index < m_HashArray.GetSize());
+	CHashTableKeyList* pHashKeyList = m_HashArray.GetElement(index);
+	if (!pHashKeyList)
+	{
+		pHashKeyList = CreateHashKeyList();
+		if (pHashKeyList)
+		{
+			if (!m_HashArray.SetElement(index,pHashKeyList))
+			{
+				ASSERT(0);
+				SafeDelete(pHashKeyList);
+				return false;
+			}
+		}
+		else
+		{
+			ASSERT(0);
+			return false;
+		}
+	}
+
+	CHashNode* pHashNode = CreateHashNode(hashKey,element);
+	if (pHashNode)
+	{
+		if (pHashKeyList->Add(pHashNode))
+		{
+			++m_Count;
+#ifndef _DEBUG
+			++m_AddCount;
+			if (pHashKeyList->GetCount() > 1)
+			{
+				++m_CollisionCount;
+			}
+#endif
+			iterator.m_Array = &m_HashArray;
+			iterator.m_ArrayIndex = index;
+			iterator.m_Iterator = pHashKeyList->Begin();
+			return true;
+		}
+		else
+		{
+			DeleteHashNode(pHashNode);
+			ASSERT(0);
+			return false;
+		}
+	}
+	else
+	{
+		ASSERT(0);
+		return false;
+	}
+}
+
+template<HashTableTemplateList>
+bool CHashTableType::Get(const CHashKey &hashKey, CIterator &iterator) const {
+	if (m_HashArray.GetSize() != 0)
+	{
+		const DWORD index = GetHashIndex(hashKey);
+		ASSERT(index < m_HashArray.GetSize());
+		CHashTableKeyList *pHashKeyList = m_HashKey.GetElement(index);
+		if (pHashKeyList)
+		{
+			typename CHashTableKeyList::CIterator list_iterator = pHashKeyList->Begin();
+			while (list_iterator != pHashKeyList->End())
+			{
+				CHashNode* pHashNode = (*list_iterator);
+				if (pHashNode->GetHashKey() == hashKey)
+				{
+					iterator.m_Array = const_cast<CHashTableArray*>(&m_HashArray);
+					iterator.m_ArrayIndex = index;
+					iterator.m_Iterator = list_iterator;
+					return true;
+				}
+				++list_iterator;
+			}
+		}
+	}
+	return false;
+}
+
+template<HashTableTemplateList>
+bool CHashTableType::Remove(CIterator iterator) {
+	CHashTableKeyList* pHashKeyList = m_HashArray.GetElement(iterator.m_ArrayIndex);
+	if (pHashKeyList)
+	{
+		CHashNode* pHashNode = *(iterator.m_Iterator);
+		pHashKeyList->Remove(iterator.m_Iterator);
+		DeleteHashNode(pHashNode);
+		if (pHashKeyList->IsEmpty())
+		{
+			m_HashArray.SetElement(iterator.m_ArrayIndex,NULL);
+			DeleteHashKeyList(pHashKeyList);
+		}
+		ASSERT(m_Count > 0);
+		--m_Count;
+		return true;
+	}
+	return false;
+}
+
 }
 
 #endif /* HASHTABLE_H_ */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
