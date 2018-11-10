d16900daf58b2c8f9000fa9b12f1f6760e1c542a
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2018-11-10 14:40:38 +0900

    Update Likely.h

--- Compared Party-Facebook/folly/Likely.h
+++ Compared Party-Facebook/folly/Likely.h
@@ -9,6 +9,34 @@
 #ifndef FOLLY_LIKELY_H_
 #define FOLLY_LIKELY_H_
 
+/*
+ * Useful when author has better knowledge than the compiler:
+ * - which code paths are designed as the fast path
+ * - which code paths are designed as the slow path
+ * Author force the compiler to optimize for the fast path,
+ * event when it is not overwhelmingly likely
+ */
 
+#if __GNUC__
+
+#define FOLLY_DETAIL_BUILTIN_EXPECT(b,t) (__builtin_expect(b,t))
+#else
+#define FOLLY_DETAIL_BUILTIN_EXPECT(b, t) b
+
+#endif
+
+#define FOLLY_LIKELY(x) FOLLY_DETAIL_BUILTIN_EXPECT((x), 1)
+#define FOLLY_UNLIKELY(x) FOLLY_DETAIL_BUILTIN_EXPECT((x), 0)
+
+#undef LIKELY
+#undef UNLIEKLY
+
+#if defined(__GNUC__)
+#define LIKELY(x) (__builtin_expect((x), 1))
+#define UNLIKELY(x) (__builtin_expect((x), 0))
+#else
+#define LIKELY(x) (x)
+#define UNLIKELY(x) (x)
+#endif
 
 #endif /* FOLLY_LIKELY_H_ */
