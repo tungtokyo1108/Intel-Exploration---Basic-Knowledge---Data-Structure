52447236f15a7138ed964fc19e68e903507da1d3
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2018-11-11 14:46:36 +0900

    Add files via upload

---
+++ Compared Party-Facebook/folly/portability/SysTypes.h
@@ -0,0 +1,21 @@
+#pragma once
+/*
+*
+*  Created on: Nov 11, 2018
+*      Student (MIG Virtual Developer): Tung Dang
+*/
+
+#include <sys/types.h>
+#ifdef _WIN32
+#include <basetsd.h>
+
+#define HAVE_MODE_T 1
+
+/*
+* This is a massive pain to have be an "int" due to the pthread implementation 
+*/
+using pid_t = int;
+using ssize_t = SSIZE_T;
+using mode_t = unsigned short;
+
+#endif 
