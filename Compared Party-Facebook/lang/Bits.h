eadd5ee243e5024d07751ba2092e9ced6c9ea2da
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2018-11-14 15:41:56 +0900

    Create Bits.h

---
+++ Compared Party-Facebook/lang/Bits.h
@@ -0,0 +1,25 @@
+/*
+ * Bits.h
+ * https://github.com/facebook/folly/blob/master/folly/lang/Bits.h
+ *
+ *  Created on: Nov 14, 2018
+ *      Student (MIG Virtual Developer): Tung Dang
+ */
+
+#ifndef FOLLY_LANG_BITS_H_
+#define FOLLY_LANG_BITS_H_
+
+#include <cassert>
+#include <cinttypes>
+#include <cstdint>
+#include <cstring>
+#include <limits>
+#include <type_traits>
+
+#include <folly/ConstexprMath.h>
+#include <folly/Portability.h>
+#include <folly/Utility.h>
+#include <folly/lang/Assume.h>
+#include <folly/portability/Builtins.h>
+
+#endif /* FOLLY_LANG_BITS_H_ */
