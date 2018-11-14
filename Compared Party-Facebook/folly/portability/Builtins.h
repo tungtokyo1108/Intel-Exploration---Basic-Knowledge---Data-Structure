1610797444fef1de0b6bba8820b89f1d8c2d352f
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2018-11-14 21:18:27 +0900

    Add files via upload

---
+++ Compared Party-Facebook/folly/portability/Builtins.h
@@ -0,0 +1,14 @@
+/*
+ * Builtins.h
+ * https://github.com/facebook/folly/blob/master/folly/portability/Builtins.h
+ *
+ *  Created on: Nov 14, 2018
+ *      Student (MIG Virtual Developer): Tung Dang
+ */
+
+#ifndef FOLLY_PORTABILITY_BUILTINS_H_
+#define FOLLY_PORTABILITY_BUILTINS_H_
+
+
+
+#endif /* FOLLY_PORTABILITY_BUILTINS_H_ */
