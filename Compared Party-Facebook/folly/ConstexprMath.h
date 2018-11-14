48319f0f971f36c358dac5050635de83e0d108e6
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2018-11-14 21:16:40 +0900

    Add files via upload

---
+++ Compared Party-Facebook/folly/ConstexprMath.h
@@ -0,0 +1,14 @@
+/*
+ * ConstexprMath.h
+ * https://github.com/facebook/folly/blob/master/folly/ConstexprMath.h
+ *
+ *  Created on: Nov 14, 2018
+ *      Student (MIG Virtual Developer): Tung Dang
+ */
+
+#ifndef FOLLY_CONSTEXPRMATH_H_
+#define FOLLY_CONSTEXPRMATH_H_
+
+
+
+#endif /* FOLLY_CONSTEXPRMATH_H_ */
---
+++ Compared Party-Facebook/folly/ScopeGuard.h
@@ -0,0 +1,14 @@
+/*
+ * ScopeGuard.h
+ * https://github.com/facebook/folly/blob/master/folly/ScopeGuard.h
+ *
+ *  Created on: Nov 13, 2018
+ *      Student (MIG Virtual Developer): Tung Dang
+ */
+
+#ifndef FOLLY_SCOPEGUARD_H_
+#define FOLLY_SCOPEGUARD_H_
+
+
+
+#endif /* FOLLY_SCOPEGUARD_H_ */
