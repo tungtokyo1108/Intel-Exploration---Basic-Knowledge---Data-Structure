64f5f23e1653394ae5d42593cc50cc459f729c2d
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2018-11-16 14:04:58 +0900

    Add files via upload

---
+++ Compared Party-Facebook/folly/hash/SpookyHashV1.h
@@ -0,0 +1,14 @@
+/*
+ * SpookyHashV1.h
+ * https://github.com/facebook/folly/blob/master/folly/hash/SpookyHashV1.h
+ *
+ *  Created on: Nov 16, 2018
+ *      Student (MIG Virtual Developer): Tung Dang
+ */
+
+#ifndef FOLLY_HASH_SPOOKYHASHV1_H_
+#define FOLLY_HASH_SPOOKYHASHV1_H_
+
+
+
+#endif /* FOLLY_HASH_SPOOKYHASHV1_H_ */
---
+++ Compared Party-Facebook/folly/hash/SpookyHashV2.h
@@ -0,0 +1,14 @@
+/*
+ * SpookyHashV2.h
+ * https://github.com/facebook/folly/blob/master/folly/hash/SpookyHashV2.h
+ *
+ *  Created on: Nov 16, 2018
+ *      Student (MIG Virtual Developer): Tung Dang
+ */
+
+#ifndef FOLLY_HASH_SPOOKYHASHV2_H_
+#define FOLLY_HASH_SPOOKYHASHV2_H_
+
+
+
+#endif /* FOLLY_HASH_SPOOKYHASHV2_H_ */
