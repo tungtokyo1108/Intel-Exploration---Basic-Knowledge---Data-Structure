185bbe40abdeb461370fa614820e3c747a54416a
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2018-11-16 14:04:32 +0900

    Create Hash.h

---
+++ Compared Party-Facebook/folly/hash/Hash.h
@@ -0,0 +1,14 @@
+/*
+ * Hash.h
+ * https://github.com/facebook/folly/blob/master/folly/hash/Hash.h
+ *
+ *  Created on: Nov 16, 2018
+ *      Student (MIG Virtual Developer): Tung Dang
+ */
+
+#ifndef FOLLY_HASH_HASH_H_
+#define FOLLY_HASH_HASH_H_
+
+
+
+#endif /* FOLLY_HASH_HASH_H_ */
