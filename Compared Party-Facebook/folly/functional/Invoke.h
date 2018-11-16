48dc73754db6c99378d74dd90e68476a8e494a98
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2018-11-16 14:05:46 +0900

    Create Invoke.h

---
+++ Compared Party-Facebook/folly/functional/Invoke.h
@@ -0,0 +1,14 @@
+/*
+ * Invoke.h
+ * https://github.com/facebook/folly/blob/master/folly/functional/Invoke.h
+ *
+ *  Created on: Nov 16, 2018
+ *      Student (MIG Virtual Developer): Tung Dang
+ */
+
+#ifndef FOLLY_FUNCTIONAL_INVOKE_H_
+#define FOLLY_FUNCTIONAL_INVOKE_H_
+
+
+
+#endif /* FOLLY_FUNCTIONAL_INVOKE_H_ */
