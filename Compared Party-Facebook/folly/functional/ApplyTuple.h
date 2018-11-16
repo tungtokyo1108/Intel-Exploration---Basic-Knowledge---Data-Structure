5e6a552965d8796acae32356fe073401581306bd
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2018-11-16 14:06:13 +0900

    Add files via upload

---
+++ Compared Party-Facebook/folly/functional/ApplyTuple.h
@@ -0,0 +1,14 @@
+/*
+ * ApplyTuple.h
+ * https://github.com/facebook/folly/blob/master/folly/functional/ApplyTuple.h
+ *
+ *  Created on: Nov 16, 2018
+ *      Student (MIG Virtual Developer): Tung Dang
+ */
+
+#ifndef FOLLY_FUNCTIONAL_APPLYTUPLE_H_
+#define FOLLY_FUNCTIONAL_APPLYTUPLE_H_
+
+
+
+#endif /* FOLLY_FUNCTIONAL_APPLYTUPLE_H_ */
